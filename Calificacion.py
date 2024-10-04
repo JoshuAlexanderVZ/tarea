e1293a983efff4f490f4966374397913d526df94
JoshuAlexanderVZ <joshuaale23@outlook.com>
2024-10-04 12:18:47 -0600

    Actualizar Calificacion.py

--- Calificacion.py
+++ Calificacion.py
@@ -1,7 +1,18 @@
+
+cal1 = int(input("Ingrese su primera calificación: "))
+cal2= int(input("Ingrese su primera calificación: "))
+cal3 = int(input("Ingrese su primera calificación: "))
+
+calificacion= (cal1+cal2+cal3)/3
+
+
 calificacion = int(input("Ingrese su calificación: "))
 if (calificacion > 100):
     print("Esta no es una calificación válida")
 elif (calificacion < 70):
     print("Reprobó")
 elif (calificacion >= 70):
+
     print("Aprobó")
+print("el promedio es: ", calificación)
+
